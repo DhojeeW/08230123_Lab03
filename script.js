7dc464dfc481ae1b62854d6404929d726afe9b2e
DhojeeW <08230123.sce@rub.edu.bt>
2025-10-06 23:54:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,17 @@ window.addEventListener('scroll', () => {
     topBtn.style.display = 'none';
   }
 });
+
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+
+  if (body.classList.contains('dark-mode')) {
+    toggleButton.textContent = '☀️ Light Mode';
+  } else {
+    toggleButton.textContent = '🌙 Dark Mode';
+  }
+});
+
